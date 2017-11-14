50208b6d94a6404e734ee612ba081a8cd8a2dfad
Ao Mo-Hellenbrand <hellenbr@in.tum.de>
2017-11-14 18:43:44 +0100

    change swe local compile script

--- cmp/swe_vm.py
+++ cmp/swe_vm.py
@@ -3,13 +3,12 @@ assertions=True
 scenario='swe'
 flux_solver='aug_riemann'
 compiler='gnu'
-#if enable iMPI, OpenMP must be disabled
+#If enable iMPI, OpenMP must be disabled
 openmp='noomp'
 mpi='default'
 impi=True
-#asagi on/off switch: accept True or False
+#Asagi on/off switch: accept True or False
 asagi=True
-#use this asagi directory when using iMPI
-asagi_dir='/media/data/emily/workspace/ihpc-install'
-#use this asagi directory when using normal MPI
-#asagi_dir='/media/DATA/install/libasagi'
+#Use this asagi directory when using iMPI
+asagi_dir='/media/data/emily/workspace/ihpcins'
+#To run with normal MPI, must provide an asagi lib dir compiled with normal MPI
